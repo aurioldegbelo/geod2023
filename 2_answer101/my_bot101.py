d366ad699b7015bd8b917e6f0574de802b8c4e83
Auriol Degbelo <auriol.degbelo@tu-dresden.de>
2023-04-03 17:40:22 +0200

    installation of a new environment

--- 2_answer101/my_bot101.py
+++ 2_answer101/my_bot101.py
@@ -1,7 +1,8 @@
-# code adapted from 
+# code adapted from https://github.com/AIAdvantage/chatgpt-api-youtube
 import openai
 import my_api_keys
 
+openai.api_key = my_api_keys.my_open_ai_key
 
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", 
                                           messages=[{"role": "user", 
