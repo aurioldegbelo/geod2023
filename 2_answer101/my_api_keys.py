f9be9feb2f5079caab49d3e79bdd31bbb10622eb
Auriol Degbelo <auriol.degbelo@tu-dresden.de>
2023-04-03 18:46:32 +0200

    test

---
+++ 2_answer101/my_api_keys.py
@@ -0,0 +1,4 @@
+# .gitignore should include reference to my_api_keys.py (the name of the ffile where we put our API keys)
+
+
+my_open_ai_key = "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"
--- 2_answer101/my_bot101.py
+++ 2_answer101/my_bot101.py
@@ -1,9 +1,8 @@
 # Code adapted from https://github.com/AIAdvantage/chatgpt-api-youtube
 # To see how to create a virtual environment, check https://python.land/virtual-environments/virtualenv 
-# python -m venv my-envi
-
+# python -m venv my-envi, and the next steps, see https://stackoverflow.com/a/74825209
 # For error of installation due to privileges, see https://stackoverflow.com/questions/66322049/could-not-install-packages-due-to-an-oserror-winerror-2-no-such-file-or-direc
-
+# For ginignore 
 import my_api_keys
 import openai
 
