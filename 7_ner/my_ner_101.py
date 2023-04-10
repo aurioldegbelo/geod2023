c4c84a3e99a6f37557f9d7a142870725dcacbedd
Auriol Degbelo <auriol.degbelo@tu-dresden.de>
2023-04-10 20:12:00 +0200

    Update my_ner_101.py

--- 7_ner/my_ner_101.py
+++ 7_ner/my_ner_101.py
@@ -3,10 +3,15 @@ import gradio as gr
 
 nlp = spacy.load("en_core_web_sm")
 
+# Code adapted from https://journal.code4lib.org/articles/15405
 def ner(sentence):
     doc = nlp(sentence)
-    ents = [(e.text, e.label_) for e in doc.ents]
-    return ents
+    places = [ent for ent in doc.ents if ent.label_ in ['GPE', 'LOC']]
 
-iface = gr.interface(fn=ner, inputs="text", outputs="text")
-iface.launch()
+    return places
+
+demo = gr.interface(fn=ner, inputs="text", outputs="text")
+demo.launch()
+
+ 
+ 
