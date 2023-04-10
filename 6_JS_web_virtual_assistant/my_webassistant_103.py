df21aa778adbc667e30bfad4d29ba010d65f8bc9
Auriol Degbelo <auriol.degbelo@tu-dresden.de>
2023-04-10 09:24:29 +0200

    folder initialized

---
+++ 6_JS_web_virtual_assistant/my_webassistant_103.py
@@ -0,0 +1,85 @@
+# Code adapted from https://github.com/AIAdvantage/chatgpt-api-youtube
+# To see how to create a virtual environment, check https://python.land/virtual-environments/virtualenv 
+# python -m venv my-envi, and the next steps, see https://stackoverflow.com/a/74825209
+# For error of installation due to privileges, see https://stackoverflow.com/questions/66322049/could-not-install-packages-due-to-an-oserror-winerror-2-no-such-file-or-direc
+# For gitignore, see https://github.com/github/gitignore/blob/main/Python.gitignore
+# If VS Code shows issues about execution policies, you may need to change the execution policies settings in the powershell, see https://www.sharepointdiary.com/2014/03/fix-for-powershell-script-cannot-be-loaded-because-running-scripts-is-disabled-on-this-system.html
+
+
+import os
+import my_api_keys
+import openai # open ai documentation at https://platform.openai.com/docs/introduction/overview
+#import gradio
+
+from llama_index import (
+    GPTKeywordTableIndex,
+    GPTSimpleVectorIndex,
+    SimpleDirectoryReader,
+    LLMPredictor,
+    ServiceContext, 
+    download_loader, 
+    PromptHelper
+)
+
+# documentation of langchain at https://github.com/hwchase17/langchain
+from langchain import OpenAI # if you want to use a model other than gpt-3.5-turbo
+
+#from langchain.chat_models import ChatOpenAI
+
+os.environ['OPENAI_API_KEY'] = my_api_keys.my_open_ai_key
+
+
+## Working with llama_index = playing around with data augmentation
+
+## Step 1: load the new data
+# documentation of llama_index at https://gpt-index.readthedocs.io/en/latest/
+# data loaders at https://llamahub.ai/
+#from llama_index import download_loader, GPTSimpleVectorIndex
+
+SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
+# Take all the files in the data folder, see https://llamahub.ai/l/file
+loader = SimpleDirectoryReader('./data', recursive=True, exclude_hidden=True)
+documents = loader.load_data()
+#print(documents)
+
+my_question = "Which countries did Cyclone Freddy affect?"
+
+
+'''
+# Simple test without customization
+index = GPTSimpleVectorIndex.from_documents(documents)
+response = index.query(my_question)
+print(response)
+'''
+
+WikipediaReader = download_loader("WikipediaReader")
+loader = WikipediaReader()
+wikidocs = loader.load_data(pages=['Cyclone Freddy'])
+wikipedia_index = GPTSimpleVectorIndex.from_documents(wikidocs)
+response = wikipedia_index.query(my_question)
+print(response)
+
+## Step 2: Build a CUSTOM llm index: code adapted from https://github.com/wombyz/custom-knowledge-chatbot/tree/main/custom-knowledge-chatbot
+# Official documentation: https://gpt-index.readthedocs.io/en/latest/how_to/customization/custom_llms.html
+
+# define prompt helper
+# set maximum input size
+max_input_size = 2048
+# set number of output tokens
+num_output = 256
+# set maximum chunk overlap
+max_chunk_overlap = 20
+prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+
+# define LLM
+llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="text-davinci-002")) #"gpt-3.5-turbo text-davinci-002"
+service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+# build index
+custom_index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+
+
+## Step 3: reuse the custom index to get some answers
+# get response from query
+response = custom_index.query(my_question)
+print(response)
