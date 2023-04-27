b3b6ebb447f5d70c65586b9a01dd8e2878fb1a21
Auriol Degbelo <auriol.degbelo@tu-dresden.de>
2023-04-27 20:27:23 +0200

    Update my_spacy_101.py

--- 7_ner/my_spacy_101.py
+++ 7_ner/my_spacy_101.py
@@ -4,8 +4,11 @@
 import spacy
 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+#doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+doc = nlp("Dresden is the capital city of the German state of Saxony and its second most populous city after Leipzig.")
+
 for token in doc:
-    print(token.text, token.pos_, token.dep_, token.shape_)
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+            token.shape_, token.is_alpha, token.is_stop)
 
 print(spacy.explain("nsubj"))
